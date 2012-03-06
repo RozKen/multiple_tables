7965569f747737f239b0ee29e18415dfab1c5ae7
Kenichi Yorozu <rozken@gmail.com>
2012-03-07 00:16:37 +0900

    created new project with sqlite3 databse

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MultipleTables::Application.config.secret_token = 'e1f7b9646240d8b8e7d7e6e2aa3556dede72fe7e27acf5c485dd5ad14da3f7464f1953f7800e0279f86fcb3bee931b5498d09027fcde8ae78b81b8066fd3deac'
